6cfd2b9e045d7d6d91af4fdb90c8dd1bb4bd9d9d
chaosweaver007 <chaosweaver007@gmail.com>
2026-07-23 11:26:08 -0400

    Include Simulation Chamber in static build

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -5,6 +5,7 @@ const outputDirectory = new URL("../dist/", import.meta.url);
 await rm(outputDirectory, { recursive: true, force: true });
 await mkdir(outputDirectory, { recursive: true });
 await cp(new URL("../index.html", import.meta.url), new URL("index.html", outputDirectory));
+await cp(new URL("../simulation.html", import.meta.url), new URL("simulation.html", outputDirectory));
 await cp(new URL("../src/", import.meta.url), new URL("src/", outputDirectory), {
   recursive: true,
 });
