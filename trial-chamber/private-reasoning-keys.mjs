1b1d9b526bfd22e1e563f52fa97ba1f2c66f9a18
chaosweaver007 <chaosweaver007@gmail.com>
2026-07-30 22:36:19 -0400

    Share private reasoning key registry

---
+++ trial-chamber/private-reasoning-keys.mjs
@@ -0,0 +1,11 @@
+export const PRIVATE_REASONING_KEYS = new Set([
+  "agent_scratchpad",
+  "agent_scratchpads",
+  "chain_of_thought",
+  "hidden_reasoning",
+  "internal_reasoning",
+  "private_reasoning",
+  "reasoning_trace",
+  "scratchpad",
+  "thoughts"
+]);
